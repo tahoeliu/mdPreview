151b0564f9dd100284dfa8c42ac7be368892db62
tahoeliu <tahoeliu@users.noreply.github.com>
2026-08-25 15:45:23 +0800

    v1.4.8: Update cask

--- Casks/mdpreview.rb
+++ Casks/mdpreview.rb
@@ -1,6 +1,6 @@
 cask "mdpreview" do
-  version "1.4.5"
-  sha256 :no_check
+  version "1.4.8"
+  sha256 "52be16522778840ad26bcdbec58930a8290505d5aca2d1c33383aab3850426c2"
 
   url "https://github.com/tahoeliu/mdPreview/releases/latest/download/mdPreview.dmg"
   name "mdPreview"
